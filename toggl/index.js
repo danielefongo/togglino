c2188af98be55225d41d2fca9354eaf4d9537ab1
danielefongo <danielefongo@gmail.com>
2019-10-17 10:38:05 +0200

    update all data on google sheet using toggl api

--- toggl/index.js
+++ toggl/index.js
@@ -1,9 +1,44 @@
 require('dotenv').config()
-let TogglClient = require('toggl-api')
+const TogglClient = require('toggl-api')
 
+let axios = require('axios');
 let toggl = new TogglClient({apiToken: process.env.TOGGL_API_TOKEN})
 let workSpaceId = process.env.TOGGL_WORKSPACE_ID
+let googleApi = "https://script.google.com/macros/s/" + process.env.GOOGLE_API_ID + "/exec"
 
-toggl.getWorkspaceProjects(workSpaceId, (err, workSpaces) => {
-    console.log(workSpaces)
-});
+doAll()
+
+async function doAll() {
+    let projectIds = await getProjectIds()
+
+    toggl.getWorkspaceProjects(workSpaceId, (err, projects) => {
+        let updatedValues = projects
+        .filter(project => projectIds.includes(project.id))
+        .map(project => sheetProject(project))
+
+        postProjects(updatedValues)
+    });
+}
+
+function sheetProject(project) {
+    actual_hours = project.actual_hours
+    if(actual_hours == undefined)
+        actual_hours = 0
+    
+        return {
+        name: project.id,
+        hours: actual_hours
+    }
+}
+
+async function getProjectIds() {
+    return await axios.get(googleApi).then(response => {
+        return response.data;
+    }).catch(console.log);
+}
+
+async function postProjects(data) {
+    return await axios.post(googleApi, data).then(response => {
+        return response.data;
+    }).catch(console.log);
+}

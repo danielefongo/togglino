2678f71693f0da002793c3566f56c8bc84fe6379
danielefongo <danielefongo@gmail.com>
2019-10-17 10:13:19 +0200

    retrieve all projects for a specific workspace

--- toggl/index.js
+++ toggl/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!")
+require('dotenv').config()
+let TogglClient = require('toggl-api')
+
+let toggl = new TogglClient({apiToken: process.env.TOGGL_API_TOKEN})
+let workSpaceId = process.env.TOGGL_WORKSPACE_ID
+
+toggl.getWorkspaceProjects(workSpaceId, (err, workSpaces) => {
+    console.log(workSpaces)
+});

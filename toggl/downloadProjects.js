98e960db5f24a2e5ee286c23161a7ba6f3f059ff
danielefongo <danielefongo@gmail.com>
2019-10-17 10:42:59 +0200

    add script to download all projects for a specific client

---
+++ toggl/downloadProjects.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const TogglClient = require('toggl-api')
+
+let toggl = new TogglClient({apiToken: process.env.TOGGL_API_TOKEN})
+let workSpaceId = process.env.TOGGL_WORKSPACE_ID
+const clientId = process.env.TOGGL_CLIENT_ID
+
+toggl.getWorkspaceProjects(workSpaceId, (err, workSpaces) => {
+   let clientProjects = workSpaces.filter((project) => project.cid == clientId)
+    for (const project of clientProjects) {
+        console.log(project.id+", "+project.name)
+    }
+});
